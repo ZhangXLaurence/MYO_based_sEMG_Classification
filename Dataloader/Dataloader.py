91a3e6c664dff8422d8f5181c2a2a9175edbecf2
Dukechris <zhangxiao1688@hotmail.com>
2018-09-18 16:13:35 +0800

    Fix

--- Dataloader/Dataloader.py
+++ Dataloader/Dataloader.py
@@ -87,7 +87,7 @@ class ImbalancedDatasetSampler(torch.utils.data.sampler.Sampler):
             return dataset.imgs[idx][1]
         elif dataset_type is CSVDataset:
             print(dataset.GetLabel(idx))
-            return dataset.GetLabel(idx).item()
+            return dataset.GetLabel(idx)[0]
         else:
             raise NotImplementedError
                 
