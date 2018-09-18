0b78c0d42300cc32328fd71b504299f6da49c9ec
Dukechris <zhangxiao1688@hotmail.com>
2018-09-19 00:20:45 +0800

    Test

--- Models/MLP.py
+++ Models/MLP.py
@@ -4,8 +4,8 @@ import torch.nn as nn
 class MLP(nn.Module):
     def __init__(self, input_dim=10, feature_dim=64, dense_dim=32):
         super(MLP, self).__init__()
-        self.conv1 = nn.Conv1d(1, 4, kernel_size=3)
-        self.fc1 = nn.Linear(4 * 8, dense_dim)
+        # self.conv1 = nn.Conv1d(1, 4, kernel_size=3)
+        self.fc1 = nn.Linear(10, dense_dim)
         self.prelu1 = nn.PReLU()
         self.fc2 = nn.Linear(dense_dim, dense_dim)
         self.prelu2 = nn.PReLU()
@@ -15,12 +15,12 @@ class MLP(nn.Module):
         # self.ip2 = nn.Linear(feature_dim, class_num)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = x.view(-1, 4*8)
+        # x = self.conv1(x)
+        # x = x.view(-1, 4*8)
         x1 = self.prelu1(self.fc1(x))
         x2 = self.prelu2(self.fc2(x1))
         x3 = self.prelu3(self.fc3(x2 + x1))
-        ip1 = self.ip1(x3)    # feature
+        ip1 = self.ip1(x3+x2+x1)    # feature
         # ip2 = self.ip2(ip1)   # logit
         return ip1
 
